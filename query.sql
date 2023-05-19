0637837946988f8289d426b6ec2dcbfa6c99814e
Cecilia <117763812+Tayokomolafe@users.noreply.github.com>
2023-05-19 16:03:52 +0100

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,103 @@
+INSERT INTO emp_manager
+SELECT 
+    u.*
+FROM
+    (SELECT 
+        a.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS a UNION SELECT 
+        b.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no > 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no
+    LIMIT 20) AS b UNION SELECT 
+        c.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110039) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 110022
+    GROUP BY e.emp_no) AS c UNION SELECT 
+        d.*
+    FROM
+        (SELECT 
+        e.emp_no AS employee_ID,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_ID
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no = 110039
+    GROUP BY e.emp_no) AS d) as u;
+    
+    
+    
+    SELECT * FROM dept_emp;
+    
+    SELECT e1. *
+    FROM emp_manager e1
+    JOIN emp_manager e2 ON e1.emp_no  = e2.manager_no;
+    
+    SELECT emp_no, from_date, to_date, COUNT(emp_no) AS Num
+    FROM dept_emp
+    GROUP BY  emp_no
+    Having NUM >1;
+    
+    CREATE VIEW v_dept_emp_latest_date AS
+    SELECT emp_no, MAX(from_date) AS FROM_date, MAX(to_date) AS to_date
+    FROM dept_emp
+    GROUP BY emp_no;
+    
+     SELECT emp_no, MAX(from_date) AS FROM_date, MAX(to_date) AS to_date
+    FROM dept_emp
+    GROUP BY emp_no;
+    
+    
+    SELECT ROUND(AVG(salary),2)
+    FROM salaries s
+    JOIN dept_manager m ON s.emp_no = m.emp_no;
